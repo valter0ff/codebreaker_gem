7804e572ae9bbc52e8f44888eba8d98b835c76a0
Valeriy.Pogrebniak <km270985pvg@bitbucket.org>
2022-01-19 23:27:41 +0200

    update classes requirements

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 require 'codebreaker/version'
+require 'codebreaker/error_class'
+require 'codebreaker/validations'
 require 'codebreaker/code_checker'
+require 'codebreaker/player'
+require 'codebreaker/game'
