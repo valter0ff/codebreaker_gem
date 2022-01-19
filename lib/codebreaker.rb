5e24a9e62336a9fb8579ba496e6a54bfcbea1af8
Valeriy.Pogrebniak <km270985pvg@bitbucket.org>
2022-01-19 23:32:20 +0200

    add requirement for code_checker class

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,8 +1,4 @@
 # frozen_string_literal: true
 
 require 'codebreaker/version'
-
-module Codebreaker
-  class Error < StandardError; end
-  # Your code goes here...
-end
+require 'codebreaker/code_checker'
