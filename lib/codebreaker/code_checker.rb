82acc5cf69e0a7299c8813a92cbb7cfb7710f822
Valeriy.Pogrebniak <km270985pvg@bitbucket.org>
2022-01-23 19:21:47 +0200

    replace "magic numbers" with constants

--- lib/codebreaker/code_checker.rb
+++ lib/codebreaker/code_checker.rb
@@ -2,6 +2,8 @@ module Codebreaker
   class CodeChecker
     PLUS = '+'.freeze
     MINUS = '-'.freeze
+    MIN_DIGIT = 1
+    MAX_DIGIT = 6
     attr_reader :secret_code, :user_input
 
     def initialize(code, input)
@@ -20,16 +22,16 @@ module Codebreaker
       user_input.each_with_index do |x, i|
         next unless secret_code[i] == x
 
-        result.push(PLUS)
         @secret_code[i] = 0
         @user_input[i] = 0
+        result.push(PLUS)
       end
       result
     end
 
     def check_minuses
       result = []
-      (1..6).each do |n|
+      (MIN_DIGIT..MAX_DIGIT).each do |n|
         next unless user_input.include?(n)
 
         match = [user_input.count(n), secret_code.count(n)].min
